68a64ddee81c006865db38ec0a69c5518bf8d990
Amanda Wehrman <arwehrman@gmail.com>
2017-11-14 20:14:44 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,12 @@ class UserController < ApplicationController
   end
 
   def create
+    user = User.new(user_params).save
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
