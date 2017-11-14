cbdbd807a03e5bf6d1bf1620de142546d28dd5cf
Amanda Wehrman <arwehrman@gmail.com>
2017-11-14 20:37:52 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,24 @@
 class UsersController < ApplicationController
+  def index
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.find_by(:name => params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
-     session[:user_id] = @user.id
-        redirect_to root_path
-       else
-      redirect_to "/login"
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      redirect_to new_user_path
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
